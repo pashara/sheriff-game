ef170e7580d989d9232c991ed901340ad30e66dc
pavel.s <pavel@epicstar.games>
2024-02-11 22:13:55 +0400

    add skybox add animator filter

--- SheriffGame/Assets/ThirdParty/NaughtyCharacter/_Scripts/CharacterAnimator.cs
+++ SheriffGame/Assets/ThirdParty/NaughtyCharacter/_Scripts/CharacterAnimator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace NaughtyCharacter
@@ -9,21 +11,47 @@ namespace NaughtyCharacter
         public static readonly int IsGrounded = Animator.StringToHash("IsGrounded");
     }
 
-    public class CharacterAnimator : MonoBehaviour
+    public class HorizontalSpeedFilter
+    {
+        private const int FrameBufferSize = 5;
+        private Queue<float> horizontalSpeedBuffer = new Queue<float>();
+
+        public float UpdateState(float horizontalSpeed)
+        {
+            // Добавляем текущее значение горизонтальной скорости в буфер
+            horizontalSpeedBuffer.Enqueue(horizontalSpeed);
+
+            // Удостоверяемся, что буфер не превышает заданный размер
+            while (horizontalSpeedBuffer.Count > FrameBufferSize)
+            {
+                horizontalSpeedBuffer.Dequeue();
+            }
+
+            // Вычисляем среднее значение горизонтальной скорости
+            float averageHorizontalSpeed = horizontalSpeedBuffer.Sum() / horizontalSpeedBuffer.Count;
+
+            return averageHorizontalSpeed;
+        }
+    }
+    
+    public class CharacterAnimator  : MonoBehaviour
     {
         private Animator _animator;
         private Character _character;
+        private HorizontalSpeedFilter _horizontalSpeedFilter;
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
             _character = GetComponent<Character>();
+            _horizontalSpeedFilter = new HorizontalSpeedFilter();
         }
 
         public void UpdateState()
         {
             float normHorizontalSpeed = _character.HorizontalVelocity.magnitude / _character.MovementSettings.MaxHorizontalSpeed;
-            _animator.SetFloat(CharacterAnimatorParamId.HorizontalSpeed, normHorizontalSpeed);
+            float filteredHorizontalSpeed = _horizontalSpeedFilter.UpdateState(normHorizontalSpeed);
+            _animator.SetFloat(CharacterAnimatorParamId.HorizontalSpeed, filteredHorizontalSpeed);
 
             float jumpSpeed = _character.MovementSettings.JumpSpeed;
             float normVerticalSpeed = _character.VerticalVelocity.y.Remap(-jumpSpeed, jumpSpeed, -1.0f, 1.0f);

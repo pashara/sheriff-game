0789fbc6a24c9f4abac67a69bd762c80dd8cac79
pavel.s <pavel@epicstar.games>
2024-02-14 03:29:42 +0400

    x

---
+++ SheriffGame/Assets/Fonts/FontConfigCopyHandler.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TextCore;
+
+namespace Fonts
+{
+    [CreateAssetMenu]
+    public class FontConfigCopyHandler : ScriptableObject
+    {
+        [SerializeField] private TMP_FontAsset assetSource;
+
+        [SerializeField] private List<Glyph> glyphs;
+
+        [Button]
+        private void Apply(TMP_FontAsset element)
+        {
+            foreach (var glyph in glyphs)
+            {
+                var index = element.glyphTable.FindIndex(x => x.index == glyph.index);
+                if (index == -1)
+                    continue;
+                
+                var g = new Glyph(element.glyphTable[index]);
+                g.metrics = new GlyphMetrics(glyph.metrics.width, glyph.metrics.height,
+                    glyph.metrics.horizontalBearingX, glyph.metrics.horizontalBearingY,
+                    glyph.metrics.horizontalAdvance);
+            }
+        }
+
+        [Button]
+        void ReadConfig()
+        {
+            glyphs = assetSource.glyphTable.Select(x => new Glyph(x)).ToList();
+        }
+    }
+}
